b43bd8a62b99ddf7fdd4bc221e2b70887ddfaf08
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-08 21:21:00 -0500

    Feat: add module of colors

---
+++ lua/modules/colorscheme/package.lua
@@ -0,0 +1,10 @@
+local package = require('core.pack').package
+local conf = require('modules.colorscheme.config')
+
+-- Kanagawa
+package({})
+
+-- Oxo Carbon
+package({
+  'nyoom-engineering/oxocarbon.nvim',
+})
