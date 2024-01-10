1a1b8049f11badcdfd471c0e6cfc5eed466a9fcb
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-10 15:41:41 -0500

    Feat: change to ronny

--- lua/modules/colorscheme/package.lua
+++ lua/modules/colorscheme/package.lua
@@ -1,7 +1,7 @@
 local package = require('core.pack').package
 local conf = require('modules.colorscheme.config')
 
-local colorscheme = 'kanagawa'
+local colorscheme = 'ronny'
 
 -- Kanagawa
 package({
@@ -10,4 +10,40 @@ package({
   priority = 1000,
   lazy = false,
   config = conf.kanagawa,
+})
+
+-- Cyberdream -- not gerat
+package({
+  enabled = colorscheme == 'cyberdream',
+  'scottmckendry/cyberdream.nvim',
+  priority = 1000,
+  lazy = false,
+  config = conf.cyberdream,
+})
+
+-- Night owl
+package({
+  enabled = colorscheme == 'night-owl',
+  'oxfist/night-owl.nvim',
+  priority = 1000,
+  lazy = false,
+  config = conf.night_owl,
+})
+
+--  Monokai nightasty
+package({
+  enabled = colorscheme == 'monokai-nightasty',
+  'polirritmico/monokai-nightasty.nvim',
+  priority = 1000,
+  lazy = false,
+  config = conf.monokai_nightasty,
+})
+
+-- Ronny
+package({
+  enabled = colorscheme == 'ronny',
+  'judaew/ronny.nvim',
+  priority = 1000,
+  lazy = false,
+  config = conf.ronny,
 })
