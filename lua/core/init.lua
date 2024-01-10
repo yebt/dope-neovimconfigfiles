542905f3ba018809244b938c885103fb0ac5defe
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-10 08:05:12 -0500

    Feat: add some default plugins

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -40,11 +40,11 @@ g.loaded_matchit = 1
 g.loaded_matchparen = 1
 g.loaded_2html_plugin = 1
 g.loaded_logiPat = 1
-g.loaded_rrhelper = 1
+-- g.loaded_rrhelper = 1
 -- g.loaded_netrw = 1
 -- g.loaded_netrwPlugin = 1
--- g.loaded_netrwSettings = 1
--- g.loaded_netrwFileHandlers = 1
+g.loaded_netrwSettings = 1
+g.loaded_netrwFileHandlers = 1
 
 require('core.pack'):boot_strap()
 require('core.options')
