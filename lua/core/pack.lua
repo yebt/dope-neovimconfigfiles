af1bbadff94d3245b6e68f39d51939a04ab98914
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-11 14:44:50 -0500

    Feat: move lazy file caall over load packages

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -67,6 +67,7 @@ function pack:boot_strap()
       },
     },
   }
+  self.lazy_file()
   self:load_modules_packages()
   lazy.setup(self.repos, opts)
 
@@ -82,6 +83,76 @@ function pack.package(repo)
     pack.repos = {}
   end
   table.insert(pack.repos, repo)
+end
+
+-- Properly load file based plugins without blocking the UI
+function pack.lazy_file()
+  pack.use_lazy_file = true
+  pack.lazy_file_events = { 'BufReadPost', 'BufNewFile', 'BufWritePre' }
+  pack.use_lazy_file = pack.use_lazy_file and vim.fn.argc(-1) > 0
+
+  -- Add support for the LazyFile event
+  local Event = require('lazy.core.handler.event')
+
+  if pack.use_lazy_file then
+    -- We'll handle delayed execution of events ourselves
+    Event.mappings.LazyFile = { id = 'LazyFile', event = 'User', pattern = 'LazyFile' }
+    Event.mappings['User LazyFile'] = Event.mappings.LazyFile
+  else
+    -- Don't delay execution of LazyFile events, but let lazy know about the mapping
+    Event.mappings.LazyFile = { id = 'LazyFile', event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' } }
+    Event.mappings['User LazyFile'] = Event.mappings.LazyFile
+    return
+  end
+
+  local events = {} ---@type {event: string, buf: number, data?: any}[]
+
+  local done = false
+  local function load()
+    if #events == 0 or done then
+      return
+    end
+    done = true
+    vim.api.nvim_del_augroup_by_name('lazy_file')
+
+    ---@type table<string,string[]>
+    local skips = {}
+    for _, event in ipairs(events) do
+      skips[event.event] = skips[event.event] or Event.get_augroups(event.event)
+    end
+
+    vim.api.nvim_exec_autocmds('User', { pattern = 'LazyFile', modeline = false })
+    for _, event in ipairs(events) do
+      if vim.api.nvim_buf_is_valid(event.buf) then
+        Event.trigger({
+          event = event.event,
+          exclude = skips[event.event],
+          data = event.data,
+          buf = event.buf,
+        })
+        if vim.bo[event.buf].filetype then
+          Event.trigger({
+            event = 'FileType',
+            buf = event.buf,
+          })
+        end
+      end
+    end
+    vim.api.nvim_exec_autocmds('CursorMoved', { modeline = false })
+    events = {}
+  end
+
+  -- schedule wrap so that nested autocmds are executed
+  -- and the UI can continue rendering without blocking
+  load = vim.schedule_wrap(load)
+
+  vim.api.nvim_create_autocmd(pack.lazy_file_events, {
+    group = vim.api.nvim_create_augroup('lazy_file', { clear = true }),
+    callback = function(event)
+      table.insert(events, event)
+      load()
+    end,
+  })
 end
 
 return pack
