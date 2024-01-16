8d66db539e90a486527227d5dbf392ee20228f4d
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-16 14:18:12 -0500

    Feat: allow load nettw, mathcin and matchparent plugins

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -36,16 +36,16 @@ g.loaded_getscript = 1
 g.loaded_getscriptPlugin = 1
 g.loaded_vimball = 1
 g.loaded_vimballPlugin = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
+-- g.loaded_matchit = 1
+-- g.loaded_matchparen = 1
 g.loaded_2html_plugin = 1
 g.loaded_logiPat = 1
 g.loaded_rrhelper = 1
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
+-- g.loaded_netrw = 1
+-- g.loaded_netrwPlugin = 1
 g.loaded_netrwSettings = 1
 g.loaded_netrwFileHandlers = 1
 
-require('core.pack'):boot_strap()
 require('core.options')
+require('core.pack'):boot_strap()
 require('keymap')
