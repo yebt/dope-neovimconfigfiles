9c3617a2518c7e9d6630102b4fb3de8683b48df7
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-16 15:55:48 -0500

    Feat: map leaders outside and before load packages

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -46,6 +46,9 @@ g.loaded_rrhelper = 1
 g.loaded_netrwSettings = 1
 g.loaded_netrwFileHandlers = 1
 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require('core.options')
 require('core.pack'):boot_strap()
 require('keymap')
