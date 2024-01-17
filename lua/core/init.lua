7f4480c1ca316eb8aaaf5f597f3ead2593304275
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-17 16:59:11 -0500

    Feat: fix call of global vars

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -46,8 +46,8 @@ g.loaded_rrhelper = 1
 g.loaded_netrwSettings = 1
 g.loaded_netrwFileHandlers = 1
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+g.mapleader = ' '
+g.maplocalleader = ' '
 
 require('core.options')
 require('core.pack'):boot_strap()
