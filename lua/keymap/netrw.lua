c0b75a1b02da529b18125b0b482bd08532d35794
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-01-23 14:09:14 -0500

    Feat: add more netrw configs

--- lua/keymap/netrw.lua
+++ lua/keymap/netrw.lua
@@ -28,7 +28,9 @@ end
 
 -- Toggle netrw
 function toggleNetrw()
-  local explorer_command = 'Vex %:p:h'
+  local explorer_command = 'Lex! %:p:h'
+  -- local explorer_command = 'Lex %:p:h'
+  -- local explorer_command = 'Vex %:p:h'
   -- local explorer_command = 'Ex %:p:h'
   local current_buf = vapi.nvim_get_current_buf()
   local current_file = vim.fn.expand('%')
@@ -57,3 +59,42 @@ end
 -----------------------------------
 
 vim.keymap.set('n', '\\', toggleNetrw, { silent = true, noremap = true, desc = 'Toggle netrw' })
+
+-- Todo
+-----------------------------------
+-- netrw-usermaps
+-- netrw_preview
+-- netrw_alto
+--
+vim.g.netrw_silen=1
+vim.g.netrw_use_errorwindow=2
+
+vim.g.netrw_banner=0 -- supress the banner
+vim.g.netrw_bannerbackslash=0
+-- 0 (reuse the same window),
+-- 1(horizontal split)
+-- 2(vertical)
+-- 3(tab)
+-- 4(preview)
+vim.g.netrw_browse_split=2
+vim.g.netrw_list_hide=vim.fn["netrw_gitignore#Hide"]()
+ -- 0:notes,1:warning,2:errors
+vim.g.netrw_errorlbl=0
+vim.g.netrw_fastbrowse=0
+vim.g.netrw_hide=0
+vim.g.netrw_keepdir=1
+-- 0(thin list one file perline)
+-- 1(long list one file perline)
+-- 2(wide lirt multiple files in line)
+-- 3(tree style)
+vim.g.netrw_liststyle=3
+
+--- Todo
+-- [ ] Implement the Rexplore (Return to netrw if a file is open from netre, return to file if netrw is open
+-- from file)
+
+--- Maps:
+-- qf   ->  file info
+
+--- Commands:
+-- du -sh file   ->  file size
