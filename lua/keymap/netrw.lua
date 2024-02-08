e5085fec04e6a99234d02041068256060449729d
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-08 08:53:03 -0500

    fix(netrw): restore netrw config

--- lua/keymap/netrw.lua
+++ lua/keymap/netrw.lua
@@ -28,7 +28,8 @@ end
 
 -- Toggle netrw
 function toggleNetrw()
-  local explorer_command = 'Lex! %:p:h'
+  --local explorer_command = 'Lex! %:p:h'
+  local explorer_command = 'Ex %:p:h'
   -- local explorer_command = 'Lex %:p:h'
   -- local explorer_command = 'Vex %:p:h'
   -- local explorer_command = 'Ex %:p:h'
@@ -66,28 +67,28 @@ vim.keymap.set('n', '\\', toggleNetrw, { silent = true, noremap = true, desc = '
 -- netrw_preview
 -- netrw_alto
 --
-vim.g.netrw_silen = 1
-vim.g.netrw_use_errorwindow = 2
-
-vim.g.netrw_banner = 0 -- supress the banner
-vim.g.netrw_bannerbackslash = 0
--- 0 (reuse the same window),
--- 1(horizontal split)
--- 2(vertical)
--- 3(tab)
--- 4(preview)
-vim.g.netrw_browse_split = 2
-vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']()
--- 0:notes,1:warning,2:errors
-vim.g.netrw_errorlbl = 0
-vim.g.netrw_fastbrowse = 0
-vim.g.netrw_hide = 0
-vim.g.netrw_keepdir = 1
--- 0(thin list one file perline)
--- 1(long list one file perline)
--- 2(wide lirt multiple files in line)
--- 3(tree style)
-vim.g.netrw_liststyle = 3
+-- vim.g.netrw_silen = 1
+-- vim.g.netrw_use_errorwindow = 2
+--
+-- vim.g.netrw_banner = 0 -- supress the banner
+-- vim.g.netrw_bannerbackslash = 0
+-- -- 0 (reuse the same window),
+-- -- 1(horizontal split)
+-- -- 2(vertical)
+-- -- 3(tab)
+-- -- 4(preview)
+-- vim.g.netrw_browse_split = 2
+-- vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']()
+-- -- 0:notes,1:warning,2:errors
+-- vim.g.netrw_errorlbl = 0
+-- vim.g.netrw_fastbrowse = 0
+-- vim.g.netrw_hide = 0
+-- vim.g.netrw_keepdir = 1
+-- -- 0(thin list one file perline)
+-- -- 1(long list one file perline)
+-- -- 2(wide lirt multiple files in line)
+-- -- 3(tree style)
+-- vim.g.netrw_liststyle = 3
 
 --- Todo
 -- [ ] Implement the Rexplore (Return to netrw if a file is open from netre, return to file if netrw is open
