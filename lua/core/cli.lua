321271edbbebbb3c4d562a025f627668d1fda3be
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-10 11:10:20 -0500

    fix(cli): format the new of the paths

--- lua/core/cli.lua
+++ lua/core/cli.lua
@@ -5,7 +5,13 @@ function cli:env_init()
   self.module_path = helper.path_join(self.config_path, 'lua', 'modules')
   self.lazy_dir = helper.path_join(self.data_path, 'lazy')
 
-  package.path = package.path .. ';' .. self.rtp .. '/lua/vim/?.lua;' .. self.module_path .. '/?.lua;'
+  -- stylua: ignore
+  package.path = package.path
+    .. ';'
+    .. self.rtp
+    .. '/lua/vim/?.lua;'
+    .. self.module_path
+    .. '/?.lua;'
   local shared = assert(loadfile(helper.path_join(self.rtp, 'lua', 'vim', 'shared.lua')))
   _G.vim = shared()
 end
