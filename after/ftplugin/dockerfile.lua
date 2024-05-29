2beb79acc00ab1fbf8de70c201253b9afc1c1181
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-05-29 16:46:48 -0500

    fix(format)

--- after/ftplugin/dockerfile.lua
+++ after/ftplugin/dockerfile.lua
@@ -1,7 +1,7 @@
-local opt=vim.opt_local
+local opt = vim.opt_local
 
-opt.sw=2
-opt.ts=2
-opt.sts=2
-opt.cin=false
-opt.et=false
+opt.sw = 2
+opt.ts = 2
+opt.sts = 2
+opt.cin = false
+opt.et = false
--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -1,5 +1,5 @@
-local opt=vim.opt_local
+local opt = vim.opt_local
 
-opt.sw=2
-opt.ts=2
-opt.sts=2
+opt.sw = 2
+opt.ts = 2
+opt.sts = 2
