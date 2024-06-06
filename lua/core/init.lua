f23900aae77bc4466630b04b3ef3805e48bd4b37
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-06 10:30:30 -0500

    format

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,10 +1,9 @@
 -- reset
 local pre_path = vim.opt.rtp
-vim.opt.rtp="~/.config/nvim"
-
+vim.opt.rtp = '~/.config/nvim'
 
 --- Load las opts
 require('core.options')
 --- set map
 --- restore rtp
-vim.opt.rtp= pre_path
+vim.opt.rtp = pre_path
