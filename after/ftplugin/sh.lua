4c4b08fd715614d9d0f6128897764031d426a961
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-17 10:54:59 -0500

    feat(after/ftplugin): add the ftplugins actions

---
+++ after/ftplugin/sh.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt_local
+
+opt.sw = 2
+opt.ts = 2
+opt.sts = 2
---
+++ after/ftplugin/vue.lua
@@ -0,0 +1,5 @@
+--
+function gotofilevue(fname)
+  return require('internal.functions').gotofile_vue(fname)
+end
+vim.opt.includeexpr = 'v:lua.gotofilevue(v:fname)'
