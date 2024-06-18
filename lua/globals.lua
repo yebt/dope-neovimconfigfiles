55e1591c4f846244770330e6fd53396735f9185f
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-18 11:08:06 -0500

    feat(globals): make change inside the object for static declarations and includee the disabled plugins for lazy

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,6 +1,24 @@
-local globals = {}
-
--- Colorscheme
-globals.colorscheme = 'habamax'
+local globals = {
+  -- Colorscheme
+  colorscheme = 'habamax',
+  -- Disabled plugins
+  desiabled_plugins = {
+    -- "editorconfig",
+    'zip',
+    'man',
+    -- "matchit",
+    -- "matchparen",
+    -- "netrwPlugin",
+    'nvim',
+    'osc52',
+    'rplugin',
+    'shada',
+    -- "spellfile",
+    'tarPlugin',
+    'tohtml',
+    'tohtml',
+    'tutor',
+  },
+}
 
 _G.globals = globals
