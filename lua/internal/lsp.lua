44c548fcc5adad9b5278e776d1dc8f0f6eba4e1b
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-19 11:43:57 -0500

    feat(internal/lsp): add notes

---
+++ lua/internal/lsp.lua
@@ -0,0 +1,12 @@
+-- some config for udpate tags on close tags with autotag
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+        underline = true,
+        virtual_text = {
+            spacing = 5,
+            severity_limit = 'Warning',
+        },
+        update_in_insert = true,
+    }
+)
