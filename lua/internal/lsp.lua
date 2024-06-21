f93cd0cc60cd27cbfef3aae66974df4aeb954a11
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-21 13:09:33 -0500

    feat(internal): add init of lsp call

--- lua/internal/lsp.lua
+++ lua/internal/lsp.lua
@@ -10,3 +10,18 @@
 --         update_in_insert = true,
 --     }
 -- )
+
+--- Omnifunc: is set to vim.lsp.omnifunciton i_C-x_C-o completion funciton
+--- tagfunction C-} C-w_], C-w_}
+--- formatexpr: set vim.slp.formatexpr to use server:
+---   - gq{motion} format with methods
+---   - gw{motion} format and restore cursor position:
+---     qwap: format the paragrap and continue where you were
+---   - nnoremap Q gq
+
+--- Maps
+-- C-] tag commands
+-- [d ]d: go to diagnostics
+-- C-w_d: open diagnostic float
+
+-- vim.lsp.set_log_level("debug") -- debug cause problems of perfomance
