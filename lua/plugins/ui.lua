1b5a429a60462d6f3128994618afca91c2be234b
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-21 13:09:23 -0500

    feat(plugins): add ui for dress and fzf

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -205,11 +205,62 @@ return {
     config = require('plugins.configs.ministarter'),
   },
 
+  --- Folds
   {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
     -- event= {"BufReadPost"},
     event = { 'VeryLazy' },
     config = require('plugins.configs.ufo'),
+  },
+
+  -- Search fzf
+  {
+    'junegunn/fzf.vim',
+    -- lazy = false,
+    cmd = {
+      'Files',
+      'Buffers',
+      'Ag',
+      'Rg',
+      'RG',
+      'Lines',
+      'BLines',
+    },
+    init = function()
+      vim.cmd([[
+      function! s:fzf_statusline()
+      " Override statusline as you like
+      highlight fzf1 ctermfg=161 ctermbg=251
+      highlight fzf2 ctermfg=23 ctermbg=251
+      highlight fzf3 ctermfg=237 ctermbg=251
+      setlocal statusline=%#fzf1#\ >\ %#fzf2#fz%#fzf3#f
+      endfunction
+
+      autocmd! User FzfStatusLine call <SID>fzf_statusline()
+      ]])
+    end,
+    dependencies = {
+      { 'junegunn/fzf', build = ':call fzf#install()' },
+    },
+  },
+  -- input and select of nvim
+  {
+    'stevearc/dressing.nvim',
+    dependencies = {
+      { 'junegunn/fzf' },
+    },
+    init = function()
+      local load = function()
+        require('dressing')
+      end
+      vim.ui.select = load
+      vim.ui.input = load
+    end,
+    opts = {
+      select = {
+        backend = { 'fzf' },
+      },
+    },
   },
 }
