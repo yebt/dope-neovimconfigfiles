a9d1c822915f16045d6b71ce01cf314d9ee337bc
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-26 10:15:37 -0500

    feat(plugins): add plugin for auto pairs and tokionight

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,1 +1,22 @@
-return {}
+return {
+  -- Autopairing
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    -- keys = {
+    --   {"(",mode="i"},
+    --   {")",mode="i"},
+    --   {"{",mode="i"},
+    --   {"}",mode="i"},
+    --   {"[",mode="i"},
+    --   {"]",mode="i"},
+    --   {"'",mode="i"},
+    --   {"`",mode="i"},
+    --   {'"',mode="i"},
+    -- },
+    lazy=false,
+    config = function()
+      require('plugins.configs.minipairs')()
+    end,
+  },
+}
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "folke/tokyonight.nvim",
+    config=function()require("plugins.configs.tokyonight")()end
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   -- load the colorscheme here
+    --   vim.cmd([[colorscheme tokyonight]])
+    -- end,
+  },
+}
