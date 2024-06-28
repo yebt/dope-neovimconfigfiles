96afead2b180d79fc87f7ce20bf19dd7633e9f7b
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-06-27 22:11:46 -0500

    feat(plugins/config): add config for mini

--- lua/plugins/configs/minipick.lua
+++ lua/plugins/configs/minipick.lua
@@ -97,5 +97,41 @@ return function()
     }
   require('mini.visits').setup()
   require('mini.extra').setup()
-  require('mini.pick').setup(opts)
+  local pick = require('mini.pick')
+  pick.setup(opts)
+
+  pick.registry.registry = function()
+    local items = vim.tbl_keys(MiniPick.registry)
+    table.sort(items)
+    local source = { items = items, name = 'Registry', choose = function() end }
+    local chosen_picker_name = MiniPick.start({ source = source })
+    if chosen_picker_name == nil then
+      return
+    end
+    return MiniPick.registry[chosen_picker_name]()
+  end
+
+  pick.registry.sessions = function()
+    local items = vim.tbl_keys(require('mini.sessions').detected)
+    local fitems = {}
+
+    for indx, el in ipairs(items) do
+      local els = vim.split(el, ' ')
+      local name = els[1]
+      local path = els[2] and "  -> " .. els[2] or ""
+      path = path:gsub('%%', '/')
+      table.insert(fitems, name .. path )
+    end
+    -- table.sort(fitems)
+    local source = { items = fitems, name = 'Sessions', choose = function() end }
+    local chosen_picker_session = MiniPick.start({ source = source })
+    -- local items = vim.tbl_keys(MiniPick.registry)
+    -- table.sort(items)
+    -- local source = { items = items, name = 'Registry', choose = function() end }
+    -- local chosen_picker_name = MiniPick.start({ source = source })
+    -- if chosen_picker_name == nil then
+    --   return
+    -- end
+    -- return MiniPick.registry[chosen_picker_name]()
+  end
 end
