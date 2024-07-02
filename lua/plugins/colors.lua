ff222eba12d54531a9c4d4b76749929d5774e447
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-02 09:19:41 -0500

    feat(plugins/colors): add some new colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -9,4 +9,29 @@ return {
     --   vim.cmd([[colorscheme tokyonight]])
     -- end,
   },
+  {
+    'NLKNguyen/papercolor-theme',
+  },
+  {
+    '2nthony/vitesse.nvim',
+    dependencies = {
+      'tjdevries/colorbuddy.nvim',
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+  },
+  {
+    "marko-cerovac/material.nvim"
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = require("plugins.configs.monokaipro")
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = require("plugins.configs.catppuccin")
+  }
+
 }
