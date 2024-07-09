1b2029f996f75446d5678590579f218e851b3b0c
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-09 15:17:24 -0500

    feat(plugins/ui): add dressing for better inputs

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -106,4 +106,22 @@ return {
     event = 'VeryLazy',
     config = require('plugins.configs.whickey'),
   },
+
+  -- Better inputs
+  {
+    'stevearc/dressing.nvim',
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require('lazy').load({ plugins = { 'dressing.nvim' } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require('lazy').load({ plugins = { 'dressing.nvim' } })
+        return vim.ui.input(...)
+      end
+    end,
+    opts = {},
+  },
 }
