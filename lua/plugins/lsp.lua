5c0e27f0ffd5bf56bfc220623804312fb889596a
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-10 08:21:37 -0500

    feat(plugins/lsp): set epo as dependency of lspconfig and add the signature managment

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,8 @@ return {
       'williamboman/mason-lspconfig.nvim',
       -- pavkages
       'williamboman/mason.nvim',
+
+      'nvimdev/epo.nvim',
     },
     event = 'VeryLazy',
     config = require('plugins.configs.lspconfig'),
@@ -32,8 +34,10 @@ return {
   {
     'nvimdev/epo.nvim',
     cond = _G.kernel.opts.plugins.completion == 'epo',
+    lazy = false,
+    priority = 10000,
     -- event = { 'InsertEnter', 'LspAttach' },
-    event = { 'LspAttach' },
+    -- event = { 'LspAttach' },
     config = require('plugins.configs.epo'),
   },
 
@@ -86,8 +90,30 @@ return {
     event = 'LspAttach',
     dependencies = {
       'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons', -- optional
+      'nvim-tree/nvim-web-devicons',     -- optional
     },
     config = require('plugins.configs.saga'),
+  },
+
+  -- Signature managment
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+      -- or, provide a table with 3 icons
+      hint_prefix = {
+        above = "↙ ", -- when the hint is on the line above the current line
+        current = "← ", -- when the hint is on the same line
+        below = "↖ " -- when the hint is on the line below the current line
+      },
+      handler_opts = {
+        border = "rounded" -- double, rounded, single, shadow, none, or a table of borders
+      },
+
+    },
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
   },
 }
