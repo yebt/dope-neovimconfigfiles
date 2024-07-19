8cf181bba72e098c597cc91faf064a916d0d8b33
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-19 16:03:25 -0500

    add queries extension for lua files

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -5,3 +5,11 @@
   "{"
   "}"
 ] @punctuation.bracket)
+
+;; extends
+
+((identifier) @namespace.builtin
+  (#eq? @namespace.builtin "vim"))
+
+((identifier) @namespace.builtin
+  (#eq? @namespace.builtin "LazyVim"))
