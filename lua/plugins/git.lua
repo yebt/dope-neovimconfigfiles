ccc5f070b79c876052c6c46a107bf8a96667ed3e
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-23 16:06:06 -0500

    feat(plugins): add git signs

---
+++ lua/plugins/git.lua
@@ -0,0 +1,28 @@
+return {
+
+  -- Signs
+  {
+    'lewis6991/gitsigns.nvim',
+    event = 'VeryLazy',
+    opts = {
+      signs = {
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+    },
+  },
+}
