816762ed540bfbaca46f5e1f44afc5f51c8eff19
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-23 14:06:47 -0500

    feat: add lazy config got bootstrap, removing tocks and other configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,5 @@
 -- Bootstrap lazy.nvim
+--
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.api.nvim_echo({ { '⃝   No lazy installed\n' }, { '●  Cloning...' } }, false, {})
@@ -17,21 +18,51 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
 -- Setup lazy.nvim
 require('lazy').setup({
+  rocks = {
+    enabled = false
+  },
+  defaults = {
+    lazy = true,
+  },
   spec = {
-    -- import your plugins
+    -- import your pluginss
     { import = 'plugins' },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { 'habamax' } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = false,
+    notify = true, -- get a notification when changes are found
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true,
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      paths = {}, -- add any custom paths here that you want to includes in the rtp
+      disabled_plugins = _kernel.options.lazy.desiabled_plugins,
+    },
+  },
+  profiling = {
+    -- Enables extra stats on the debug tab related to the loader cache.
+    -- Additionally gathers stats about all package.loaders
+    loader = false,
+    -- Track each new require in the Lazy profiling tab
+    require = true,
+  },
 })
