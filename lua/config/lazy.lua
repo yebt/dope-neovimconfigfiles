9710ba73559bc5aad3eb879513aeaa1e12fff680
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-07-26 10:12:03 -0500

    feat(config): fix the lazy starter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,7 +2,10 @@
 --
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.api.nvim_echo({ { '⃝   No lazy installed\n' }, { '●  Cloning...' } }, false, {})
+  vim.api.nvim_echo({
+    { 'No lazy.nvim installed:\n', 'WarningMsg' },
+    { 'Cloning\n', 'WarningMsg' },
+  }, true, {})
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
