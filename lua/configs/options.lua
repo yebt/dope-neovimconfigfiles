960433da45d69f7ea279ad6fd06725c7e42b5299
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-08 18:46:17 -0500

    feat: remove old lazy un used lazy file and change some jeys

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -75,6 +75,7 @@ local options = {
 
   ruler = true,
   showtabline = 2, -- always
+  tabpagemax = 15,
   -- tabline='%0*%#TabLine#%#TabLineSel#> %<%f %h%m%r %0*'
   -- tabline='%#TabLine# %#TabLineSel#> %<%f %#WarningMsg#%h%m%r %#TabLineFill#',
   -- laststatus = 3,
@@ -266,3 +267,87 @@ end
 vopt.grepformat = '%f:%l:%c:%m,%f:%l:%m'
 vopt.grepprg = 'rg --vimgrep --no-heading --smart-case'
 --end
+-- vim.cmd([[
+--
+-- set tabline=%!MyTabLine()
+-- if exists("+showtabline")
+--     function! MyTabLine()
+--         let s = ''
+--         let wn = ''
+--         let t = tabpagenr()
+--         let i = 1
+--         while i <= tabpagenr('$')
+--             let buflist = tabpagebuflist(i)
+--             let winnr = tabpagewinnr(i)
+--             let s .= '%' . i . 'T'
+--             let s .= (i == t ?  '%1*' : '%2*')
+--             let s .= ' '
+--             let wn = tabpagewinnr(i,'$')
+--
+--             let s .= (i== t ? '%#TabNumSel#' : '%#TabNum#')
+--             let s .= i
+--             if tabpagewinnr(i,'$') > 1
+--                 let s .= '.'
+--                 let s .= (i== t ? '%#TabWinNumSel#' : '%#TabWinNum#')
+--                 let s .= (tabpagewinnr(i,'$') > 1 ? wn : '')
+--             end
+--
+--             let s .= ' %*'
+--             let s .= (i == t ? '%#TabLineSel#' : '%#TabLine#')
+--             let bufnr = buflist[winnr - 1]
+--             let file = bufname(bufnr)
+--             let buftype = getbufvar(bufnr, 'buftype')
+--             if buftype == 'nofile'
+--                 if file =~ '\/.'
+--                     let file = substitute(file, '.*\/\ze.', '', '')
+--                 endif
+--             else
+--                 let file = fnamemodify(file, ':p:t')
+--             endif
+--             if file == ''
+--                 let file = '[No Name]'
+--             endif
+--             let s .= file
+--             let s .= (i == t ?  '%m' : '')
+--             let i = i + 1
+--         endwhile
+--         let s .= '%T%#TabLineFill#%='
+--         return s
+--     endfunction
+--     set stal=2
+--     set tabline=%!MyTabLine()
+-- endif
+--
+-- "function MyTabLabel(n)
+-- "  let buflist = tabpagebuflist(a:n)
+-- "  let winnr = tabpagewinnr(a:n)
+-- "  return bufname(buflist[winnr - 1])
+-- "endfunction
+-- "function MyTabLine()
+-- "  let s = ''
+-- "  for i in range(tabpagenr('$'))
+-- "    " select the highlighting
+-- "    if i + 1 == tabpagenr()
+-- "      let s ..= '%#TabLineSel#'
+-- "    else
+-- "      let s ..= '%#TabLine#'
+-- "    endif
+-- "
+-- "    " set the tab page number (for mouse clicks)
+-- "    let s ..= '%' .. (i + 1) .. 'T'
+-- "
+-- "    " the label is made by MyTabLabel()
+-- "    let s ..= ' %{MyTabLabel(' .. (i + 1) .. ')} '
+-- "  endfor
+-- "
+-- "  " after the last tab fill with TabLineFill and reset tab page nr
+-- "  let s ..= '%#TabLineFill#%T'
+-- "
+-- "  " right-align the label to close the current tab page
+-- "  if tabpagenr('$') > 1
+-- "    let s ..= '%=%#TabLine#%999Xclose'
+-- "  endif
+-- "
+-- "  return s
+-- "endfunction
+-- ]])
