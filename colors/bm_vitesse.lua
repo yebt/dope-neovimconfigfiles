3cd550afc666f18434e56d0425f3b1fa4ac7f068
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-08 18:44:27 -0500

    feat: add colors from mini base 64

---
+++ colors/bm_vitesse.lua
@@ -0,0 +1,82 @@
+-- Black Metal (vitesse)
+  -- primary = "#4d9375",
+  --
+  -- foreground = "#c8c5b8",
+  -- activeForeground = "#bebaab",
+  -- secondaryForeground = "#83827d",
+  -- ignored = "#383837",
+  -- border = "#f0f0f0",
+  --
+  -- background = "#121212",
+  -- activeBackground = "#181818",
+  --
+  -- lowBackground = "#222222",
+  -- lowActiveBackground = "#292929",
+  -- lowBorder = "#E7E5DB",
+  --
+  -- comment = "#5c6b5e",
+  -- string = "#c98a7d",
+  -- variable = "#bd976a",
+  -- keyword = "#4d9375",
+  -- number = "#4C9A91",
+  -- boolean = "#4d9375",
+  -- operator = "#cb7676",
+  -- func = "#80a665",
+  -- constant = "#c99076",
+  -- class = "#6893BF",
+  -- interface = "#5DA994",
+  -- type = "#5da9a7",
+  -- builtin = "#cb7676",
+  -- property = "#b8a965",
+  -- namespace = "#db889a",
+  -- punctuation = "#666666",
+  -- decorator = "#bd8f8f",
+  -- regex = "#c4704f",
+  --
+  -- base00: "#000000"
+  -- base01: "#121212"
+  -- base02: "#222222"
+  -- base03: "#333333"
+  -- base04: "#999999"
+  -- base05: "#c1c1c1"
+  -- base06: "#999999"
+  -- base07: "#c1c1c1"
+  -- base08: "#5f8787"
+  -- base09: "#aaaaaa"
+  -- base0A: "#79241f"
+  -- base0B: "#f8f7f2"
+  -- base0C: "#aaaaaa"
+  -- base0D: "#888888"
+  -- base0E: "#999999"
+  -- base0F: "#444444"
+  --
+  -- -- colors
+  -- green = "#4d9375",
+  -- cyan = "#5eaab5",
+  -- blue = "#6394bf",
+  -- red = "#cb7676",
+  -- orange = "#d4976c",
+  -- yellow = "#e6cc77",
+  -- magenta = "#d9739f",
+  --
+require('mini.base16').setup({
+  palette = {
+    base00 = '#121212', --- bg
+    base01 = '#181818', --- col
+    base02 = '#222222', --- selection
+    base03 = '#5c6b5e', --- comment
+    base04 = '#e6cc77', --- number in column
+    base05 = '#f8f7f2', --- property of the list
+    base06 = '#999999', -- unknow
+    base07 = '#c1c1c1', -- unknow
+    base08 = '#79241f', --- Error msg
+    base09 = '#aaaaaa', -- unknow
+    base0A = '#e78a53', -- n in serarch,
+    base0B = '#c98a7d', --- string and tab text color
+    base0C = '#4d9375', --- pharentesis and numcol
+    base0D = '#80a665', --- setup woord
+    base0E = '#999999', -- unknow
+    -- base0F = '#666666', --- Puntuations
+    base0F = '#6394bf', --- Puntuations
+  },
+})
