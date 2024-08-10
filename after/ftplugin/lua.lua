4f130c165b0e72f3bd73c5dec8e4b15d2dc7cf99
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-10 12:45:14 -0500

    feat: try fix the gf

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -95,5 +95,6 @@ function find_required_path(module)
 end
 
 -- Set options to open require with gf
+-- vim.opt_local.isfname = ""
 vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
 vim.opt_local.includeexpr = 'v:lua.find_required_path(v:fname)'
