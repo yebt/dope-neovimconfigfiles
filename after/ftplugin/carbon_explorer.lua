496ad775a166549b89328848c3d55d98c41c0ab7
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-08-10 12:21:56 -0500

    feat: fix settings for carbon windows

---
+++ after/ftplugin/carbon_explorer.lua
@@ -0,0 +1,8 @@
+--vim.cmd("q")
+vim.schedule(function()
+  vim.opt_local.nu = false
+  vim.opt_local.rnu = false
+  vim.opt_local.cul = true
+  -- vim.opt_local.scl = "no"
+  vim.opt_local.fdc = "0"
+end)
